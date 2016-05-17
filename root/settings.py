7763fa65a8925fceb0028e39154cc72cc7ffb496
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-05-17 18:21:41 +0300

    add static_root

--- root/settings.py
+++ root/settings.py
@@ -140,5 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/data/work/virtualenvs/runstat/src/static'
 
 # load other settings
