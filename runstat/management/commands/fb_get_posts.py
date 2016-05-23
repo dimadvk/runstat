e645376df20fe2929d48b05e0fcc6ba2be68f699
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-05-23 19:00:11 +0300

    command fb_get_posts works

--- runstat/management/commands/fb_get_posts.py
+++ runstat/management/commands/fb_get_posts.py
@@ -1,17 +1,17 @@
 """Management command for getting posts from feed of facebook group."""
 import os
+import re
 import json
 import facebook
 import requests
-from optparse import make_option
 import dateutil.parser
 from pytz import utc
-import re
+from datetime import timedelta
 
 from django.core.management import BaseCommand
 from django.conf import settings
 
-from runstat.models import GroupPost, PostAttachments, MemberTag
+from runstat.models import GroupPost, PostAttachments, MemberTag, GroupMember
 
 
 class Command(BaseCommand):
@@ -19,23 +19,26 @@ class Command(BaseCommand):
 
     # TODO: have to cpecify options to set 'since' parametr
     # without 'since' specified script gets last 'updated time' from db
-    args = '--since=UNIXTIME, --limit=NUM'
+    # args = '<--since=UNIXTIME, --limit=NUM>'
+    # args = ['--since=UNIXTIME',]
     help = "Get posts from feed of facebook group."
 
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '--since',
-            action='store',
-            dest='since',
-            help='Unixtime posts should be get from"',
-        ),
-        make_option(
+    def add_arguments(self, parser):
+        """Add optional arguments for command."""
+        parser.add_argument(
             '--limit',
             action='store',
             dest='limit',
+            default=False,
             help='Maximum number of posts per page in API-query',
         )
-    )
+        parser.add_argument(
+            '--since',
+            action='store',
+            dest='since',
+            default=False,
+            help='Maximum number of posts per page in API-query',
+        )
 
     BASE_DIR = settings.BASE_DIR
     TOKEN_VALIDATION_URL = 'https://graph.facebook.com/oauth/access_token_info'
@@ -52,6 +55,7 @@ class Command(BaseCommand):
     APP_ID = SECRETS['FACEBOOK_APP']['APP_ID']
     APP_SECRET = SECRETS['FACEBOOK_APP']['APP_SECRET']
     GROUP_ID = SECRETS['FACEBOOK_GROUP_ID']
+    POSTS_NUM_LIMIT = '100'
 
     def _get_graph_object(self):
         """Return instance of GraphAPI with access tocken set."""
@@ -112,19 +116,21 @@ class Command(BaseCommand):
         # get limit option
         limit = options.get('limit')
         if not limit:
-            limit = 100
+            limit = self.POSTS_NUM_LIMIT
         # get 'since' option
         since = options.get('since')
         # if not since get 'updated time' of last post in database
         if not since:
             try:
                 last_post = GroupPost.objects.order_by('-updated_time')[0]
-                since = last_post.updated_time.strftime('%s')
+                since = last_post.updated_time - timedelta(hours=1)
+                since = since.strftime('%s')
             except:
                 since = '0'
+        print "Options: limit={}, since={}".format(limit, since)
         # make a query to facebook with 'since' parametr
         fields = 'id,from,updated_time,created_time,message,attachments'
-        kwargs = {'fields': fields, 'limit': 100, 'since': since}
+        kwargs = {'fields': fields, 'limit': limit, 'since': since}
         posts_page = graph.get_connections(
             id=self.GROUP_ID, connection_name='feed', **kwargs)
         posts_list = posts_page.get('data', [])
@@ -142,9 +148,9 @@ class Command(BaseCommand):
             # check attachment
             attachments = self._get_attachments(post)
             created_time = dateutil.parser.parse(post.get('created_time'))
-            created_time = created_time.astimezone(utc).replace(tzinfo=None)
+            # created_time = created_time.astimezone(utc).replace(tzinfo=None)
             updated_time = dateutil.parser.parse(post.get('updated_time'))
-            updated_time = updated_time.astimezone(utc).replace(tzinfo=None)
+            # updated_time = updated_time.astimezone(utc).replace(tzinfo=None)
             message = post.get('message', '')
             message = message.encode('utf-8')
             posts_pretty_list.append({
@@ -157,36 +163,66 @@ class Command(BaseCommand):
             })
         return posts_pretty_list
 
+    def _write_attachments_info(self, fb_post, db_post):
+        """Write to database info about attachments of post."""
+        links = fb_post['attachments']['links']
+        if len(links) == 0:
+            PostAttachments.objects.create(
+                post=db_post,
+                title=fb_post['attachments']['title']
+            )
+        else:
+            for link in links:
+                PostAttachments.objects.create(
+                    post=db_post,
+                    url=link,
+                    title=fb_post['attachments']['title']
+                )
+
+    def _write_posts_tags(self, posts):
+        """Collect tags from posts messages and write to database."""
+        # make list of dicts {'author': 'tags'}
+        members_tags = []
+        for post in posts:
+            tags = re.findall(
+                re.compile(r'\#(\w+)', re.IGNORECASE|re.U),
+                post['message'].decode('utf8'),
+            )
+            members_tags.append(
+                {'author': post['author'],
+                 'tags': tags}
+            )
+        # write tags to database
+        for el in members_tags:
+            for tag in el['tags']:
+                try:
+                    MemberTag.objects.create(
+                        author=GroupMember.objects.get(object_id=el['author']),
+                        tag=tag,
+                    )
+                except:
+                    continue
+
     def handle(self, *args, **options):
         """Get posts from group feed and write to database."""
         # get posts_list from facebook group
         posts_list = self._get_posts_from_fb(options)
         # write received posts to database
         for fb_post in posts_list:
-            db_post, created = GroupPost.objcets.update_or_create(
-                object_id=post['object_id'],
-                author=post['author'],
-                created_time=post['created_time'],
-                updated_time=post['updated_time'],
-                message=post['message'])
+            # try:
+            db_post, created = GroupPost.objects.update_or_create(
+                object_id=fb_post['object_id'],
+                author=GroupMember.objects.get(object_id=fb_post['author']),
+                defaults={
+                    'created_time': fb_post['created_time'],
+                    'updated_time': fb_post['updated_time'],
+                    'message': fb_post['message'],
+                }
             )
+            # except:
+            #     continue
             # and write attachments info to database
-            links = fb_post['attachments']['links']
-            if len(links) == 0:
-                db_curs.execute(
-                    """insert into runstat_postattachments (post_id, title)
-                        values (%s, %s)""",
-                    (post_id, post['attachments']['title'])
-                )
-                PostAttachments.
-            else:
-                for link in links:
-                    db_curs.execute(
-                        """insert into runstat_postattachments
-                            (post_id, url, title) values (%s, %s, %s)""",
-                        (post_id,
-                         link,
-                         post['attachments']['title'])
-                    )
+            if created:
+                self._write_attachments_info(fb_post, db_post)
         # # write posts tags
-        write_posts_tags(posts_list)
+        self._write_posts_tags(posts_list)
